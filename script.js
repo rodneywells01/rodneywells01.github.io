4f0682a711cb104c88bcdf165b07648631a5d09f
rodneywells01 <rodneywells01@gmail.com>
2016-03-03 22:31:00 -0500

    hover, scroll buttons, navbarhighlight
    - Removed old hover library
    - Added new hover library through bower
    - Clean up naming convention
    - Added new images for about section
    - Added (empty) other sections
    - Added scrolling nav bar highlight

--- script.js
+++ script.js
@@ -3,10 +3,41 @@ $(document).ready(function() {
 
 	var collagecolors = ["aliceblue", "antiquewhite", "cadetblue", "cornsilk", "darkcyan", "darkgoldenrod", "darksalmon", "darkslategrey", "darkseagreen", "forestgreen", "goldenrod", "lightblue", "lightgreen", "lightseagreen", "mediumpurple"];
 
-	console.log(collageblocks.length);
 	$.each(collageblocks, function(index, block) {
 		randnum = Math.floor(Math.random() * collagecolors.length);
 		$(block).css("background-color", collagecolors[randnum]);
 	});
 
+	$(window).scroll(function() {
+		update_navbar();
+	});
+
+	$(window).load(function() {
+		$("#navbarhighlight").fadeIn('fast');
+		update_navbar();
+	});
+	
 });
+
+
+function update_navbar() {
+	var widthofname = $("#mynamecontainer").width();		
+	var width = document.documentElement.clientWidth - 400 / $(window).height();
+	
+	// Pixel contribution relativetop 
+	var sizeoftop = $("#navbar").height() + $("#topcardcontainer").height();			
+	var relativetop = sizeoftop / $(document).height();
+	var reltoppixels = relativetop * (width - widthofname);	
+
+
+	var percentage = ($(window).scrollTop() / ($(document).height() - $(window).height()));			
+	var leftpos = widthofname - reltoppixels // 100% right
+	+ (width - widthofname + reltoppixels - $("#navbarhighlight").width()) * percentage; // Pixel Contribution of percentage 
+	
+	$("#navbarhighlight").css("left", leftpos);
+}
+
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
